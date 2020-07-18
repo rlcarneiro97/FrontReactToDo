2164afae322f2783ab04fc342cc8ea144c3044b4
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-07-18 20:38:07 -0300

    projeto finalizado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./style.css"
 
-const Header = () => <header id="main-header">Todo List</header>
+const Header = () => <header id="main-header">TODO LIST</header>
 
 export default Header
