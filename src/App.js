d0acd06fd67da774d3b964c186bcf4810968bda2
rlcarneiro97 <rafaeldelima97@hotmail.com>
2020-07-21 15:04:15 -0300

    arrumando diretorio do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "./components/Header"
-import "./style.css"
-import Routes from "./routes"
+import "./styles/style.css"
+import Routes from "./routes/routes"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
